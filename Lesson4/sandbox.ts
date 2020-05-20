50999992b4b1b020a8a28ecad0490ae66419bdb4
Tae Hui Chang <jasmine878@yahoo.com>
2020-05-19 22:07:18 -0500

    create arrays of single type and mixed types

---
+++ Lesson4/sandbox.ts
@@ -0,0 +1,22 @@
+// arrays
+let names = ['luigi', 'mario', 'yoshi'];
+
+names.push('toad');
+
+//cannot push a number to a string array
+// names.push(3);
+
+let numbers = [10, 20, 30, 40];
+
+numbers.push(25);
+
+//cannot push a string to a numbers array
+// numbers.push('shaun');
+
+let mixedArr = ['ken', 4, 'chun-li', 8, 9];
+
+mixedArr.push('ryu');
+mixedArr.push(10);
+mixedArr[0] = 3;
+
+//objects
