09bed9539251ebdc19e83b13c905120385f720de
Tae Hui Chang <jasmine878@yahoo.com>
2020-06-08 22:13:33 -0500

    explicitly type a primitive variable

---
+++ Lesson5/sandbox.ts
@@ -0,0 +1,16 @@
+// explicit types
+let character: string;
+let age: number;
+let isLoggedIn: boolean;
+
+// age = 'luigi';
+age = 30;
+
+// isLoggedIn = 25;
+isLoggedIn = true;
+
+// arrays
+
+// union types
+
+// objects
