7143168c870fd1bb6a4f442ac525555ddc612613
Tae Hui Chang <jasmine878@yahoo.com>
2020-06-11 18:55:02 -0500

    create a function with a default parameter

--- Lesson8/sandbox.js
+++ Lesson8/sandbox.js
@@ -3,8 +3,19 @@ var greet = function () { return console.log('hello, world'); };
 //explicitly type as Function
 var greet2;
 greet2 = function () { return console.log('hello, again'); };
-//function with optional parameter
+//function with an optional parameter
 var add = function (a, b, c) {
     console.log(a + b);
 };
+console.log("optional parameter - ");
 add(5, 10);
+//function with a default parameter
+var add2 = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a + b);
+    console.log(c);
+};
+console.log("default parameter - ");
+add2(5, 10);
+console.log("default parameter - ");
+add2(5, 10, 30);
--- Lesson8/sandbox.ts
+++ Lesson8/sandbox.ts
@@ -6,9 +6,21 @@ let greet2: Function;
 
 greet2 = () => console.log('hello, again');
 
-//function with optional parameter
+//function with an optional parameter
 const add = (a: number, b: number, c?: number | string) => {
   console.log(a + b);
 }
 
+console.log("optional parameter - ")
 add(5, 10);
+
+//function with a default parameter
+const add2 = (a: number, b: number, c: number | string = 10) => {
+  console.log(a + b);
+  console.log(c);
+}
+
+console.log("default parameter - ")
+add2(5, 10);
+console.log("default parameter - ")
+add2(5, 10, 30);
