94dbb5d383692fbad6975955272d1e112c41b6b7
Tae Hui Chang <jasmine878@yahoo.com>
2020-06-12 16:19:18 -0500

    Access the href of an anchor element

--- Lesson11/public/app.js
+++ Lesson11/public/app.js
@@ -1,3 +1,8 @@
 "use strict";
 var anchor = document.querySelector('a');
 // console.log(anchor.href);
+if (anchor) {
+    console.log(anchor.href);
+}
+var anchor1 = document.querySelector('a');
+console.log(anchor1.href);
--- Lesson11/src/app.ts
+++ Lesson11/src/app.ts
@@ -1,3 +1,12 @@
 const anchor = document.querySelector('a');
 
 // console.log(anchor.href);
+
+if (anchor) {
+  console.log(anchor.href);
+}
+
+const anchor1 = document.querySelector('a')!;
+
+console.log(anchor1.href);
+
