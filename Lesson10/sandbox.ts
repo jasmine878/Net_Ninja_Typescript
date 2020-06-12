8652df7624f1f9c1191758f9e9c30cbbccdb3bad
Tae Hui Chang <jasmine878@yahoo.com>
2020-06-12 13:47:37 -0500

    Create several functions using function signatures

---
+++ Lesson10/sandbox.ts
@@ -0,0 +1,29 @@
+//set variable type as function
+let greet: Function;
+
+//Function signature
+
+//example 1
+let greet1: (a: string, b: string) => void;
+
+greet1 = (name: string, greeting: string) => {
+  console.log(`${name} says ${greeting}`);
+}
+
+//example 2
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (numOne: number, numTwo: number, action: string) => {
+  if (action === 'add') {
+    return numOne + numTwo;
+  } else {
+    return numTwo - numTwo;
+  }
+}
+
+//example 3
+let logDetails: (obj: {name: string, age: number}) => void;
+
+logDetails = (ninja: {name: string, age: number}) => {
+  console.log(`${ninja.name} is ${ninja.age} years old`);
+}
