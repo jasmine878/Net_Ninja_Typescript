0e4ee822ea7bc7f31127433ad16884855f76bdb8
Tae Hui Chang <jasmine878@yahoo.com>
2020-06-11 22:27:11 -0500

    Create another Type Alias

--- Lesson9/sandbox.ts
+++ Lesson9/sandbox.ts
@@ -1,13 +1,14 @@
 type StringOrNum = string | number;
+type objWithName = { name: string, uid: StringOrNum}
 
 const logDetails = (uid: StringOrNum, item: string) => {
   console.log(`${item} has a uid of ${uid}`);
 }
 
-const greet = (user: {name: string, uid: StringOrNum}) => {
+const greet = (user: objWithName) => {
   console.log(`${user.name} says hello`);
 }
 
-const greetAgain = (user: { name: string, uid: StringOrNum}) => {
+const greetAgain = (user: objWithName) => {
   console.log(`${user.name} says hello`);
 }
