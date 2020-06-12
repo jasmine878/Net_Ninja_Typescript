a9b6fe0bd6f91d0733e075da59bfc8cfc7f672ac
Tae Hui Chang <jasmine878@yahoo.com>
2020-06-11 22:11:18 -0500

    create 3 functions with long-winded parameters

---
+++ Lesson9/sandbox.ts
@@ -0,0 +1,11 @@
+const logDetails = (uid: string | number, item: string) => {
+  console.log(`${item} has a uid of ${uid}`);
+}
+
+const greet = (user: {name: string, uid: string | number}) => {
+  console.log(`${user.name} says hello`);
+}
+
+const greetAgain = (user: { name: string, uid: string | number }) => {
+  console.log(`${user.name} says hello`);
+}
