6f9e0dcbaf2f0dd8ec28ffacde959705c8858606
Tae Hui Chang <jasmine878@yahoo.com>
2020-06-13 17:54:50 -0500

    Setup Lesson 13

---
+++ Lesson13/app.ts
@@ -0,0 +1,25 @@
+//classes
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+
+  constructor(client: string, details: string, amount: number) {
+    this.client = client;
+    this.details = details;
+    this.amount = amount;
+  }
+
+  format() {
+    return `${this.client} owes £${this.amount} for ${this.details}`;
+  }
+}
+
+const invoice1 = new Invoice('mario', 'work on the mario website', 250);
+const invoice2 = new Invoice('luigi', 'work on the luigi website', 300);
+
+let invoices: Invoice[] = [];
+
+invoices.push(invoice1);
+invoices.push(invoice2);
+
