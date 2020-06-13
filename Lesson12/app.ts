d7bafdf2d648fc19fc2731f0da9fee0914efc7eb
Tae Hui Chang <jasmine878@yahoo.com>
2020-06-13 16:19:19 -0500

    Create an Invoice Class

---
+++ Lesson12/app.ts
@@ -0,0 +1,17 @@
+//classes
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+
+  constructor(client: string, details: string, amount: number) {
+    this.client = client;
+    this.details = details;
+    this.amount = amount;
+  }
+
+  format() {
+    return `${this.client} owes £${this.amount} for ${this.details}`;
+  }
+
+}
