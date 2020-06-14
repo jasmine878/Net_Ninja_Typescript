c888cdc35d2dc52c599f984953079ae12fa1ac0c
Tae Hui Chang <jasmine878@yahoo.com>
2020-06-13 21:24:14 -0500

    Setup Lesson14

---
+++ Lesson14_FULL/public/app.js
@@ -0,0 +1,20 @@
+"use strict";
+var Invoice = /** @class */ (function () {
+    function Invoice(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes \u00A3" + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invoice1 = new Invoice('mario', 'work on the mario website', 250);
+var invoice2 = new Invoice('luigi', 'work on the luigi website', 300);
+var invoices = [];
+invoices.push(invoice1);
+invoices.push(invoice2);
+invoices.forEach(function (invoice) {
+    console.log(invoice.client, invoice.amount, invoice.format());
+});
---
+++ Lesson14_FULL/src/app.ts
@@ -0,0 +1,23 @@
+class Invoice {
+  constructor(
+    readonly client: string,
+    private details: string,
+    public amount: number
+  ) {}
+
+  format() {
+    return `${this.client} owes £${this.amount} for ${this.details}`;
+  }
+}
+
+const invoice1 = new Invoice('mario', 'work on the mario website', 250);
+const invoice2 = new Invoice('luigi', 'work on the luigi website', 300);
+
+let invoices: Invoice[] = [];
+
+invoices.push(invoice1);
+invoices.push(invoice2);
+
+invoices.forEach(invoice => {
+  console.log(invoice.client, invoice.amount, invoice.format());
+})
