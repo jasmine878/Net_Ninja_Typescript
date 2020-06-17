c86b04cb712120fa5ef43e6b9531bff328f896bf
Tae Hui Chang <jasmine878@yahoo.com>
2020-06-17 00:14:46 -0500

    Create a regular function

---
+++ Lesson18/app.ts
@@ -0,0 +1,12 @@
+//regular function
+const addUID = (obj: object) => {
+  let uid = Math.floor(Math.random() * 100);
+
+  return {...obj, uid};
+}
+
+let docOne = addUID({ name: 'yoshi', age: 40 });
+
+console.log(docOne);
+
+//Generics
