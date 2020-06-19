7634ee63d9a7203d82bcc2034f367f2b71d00907
Tae Hui Chang <jasmine878@yahoo.com>
2020-06-19 00:09:40 -0500

    Setup Lesson20

---
+++ Lesson20_FULL/public/app.js
@@ -0,0 +1,25 @@
+import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/Payment.js';
+import { ListTemplate } from './classes/ListTemplate.js';
+//Typecasting
+const form = document.querySelector('.new-item-form');
+//inputs
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+//unordered list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
+//create an event listener to the form
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, 'end');
+});
---
+++ Lesson20_FULL/src/app.ts
@@ -0,0 +1,33 @@
+import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/Payment.js';
+import { HasFormatter } from './interfaces/HasFormatter';
+import { ListTemplate } from './classes/ListTemplate.js';
+
+//Typecasting
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+//inputs
+const type = document.querySelector('#type') as HTMLSelectElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+//unordered list template instance
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
+
+//create an event listener to the form
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+
+  let doc: HasFormatter;
+
+  if (type.value === 'invoice') {
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+  } else {
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+  }
+
+  list.render(doc, type.value, 'end');
+})
+
